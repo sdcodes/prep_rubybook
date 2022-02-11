0e0f14dddaef6669aeb0f31efabc79192742dfde
Santi D <santid13@hotmail.com>
2022-02-11 00:02:37 +0000

    testing something

---
+++ methods/samples.rb
@@ -0,0 +1,22 @@
+a = 5
+def some_method
+  a = 3
+end
+
+puts a
+
+#value of a on line 6 will equal 5 because
+#method definition creates their own scope
+#that's entirely outside of the execution flow
+
+[1, 2, 3].each do |num|
+  puts num
+end 
+# method invocation 
+
+# VS
+
+def print_num(num)
+  puts num
+end 
+#method definition 
--- methods/say.rb
+++ methods/say.rb
@@ -2,3 +2,12 @@ puts "hello"
 puts "hi"
 puts "how are you"
 puts "i'm fine"
+
+def say(words="hello")
+  put words + "."
+end 
+
+say("")
+say("hi")
+say("how are you")
+say("I'm fine")
