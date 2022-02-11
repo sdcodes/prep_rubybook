7e3fe289dac0038118f53abe2955e6d9417f1bbe
Santi D <santid13@hotmail.com>
2022-02-11 21:40:33 +0000

    ples throughout the chapter part 1
    Signed-off-by: Santi D <santid13@hotmail.com>

---
+++ loops_iterators/countdown.rb
@@ -0,0 +1,15 @@
+x = gets.chomp.to_i
+while x >= 0
+  puts x
+  x -= 1
+end
+
+puts "Done!"
+
+y = gets.chomp.to_i
+until x < 0
+puts x
+x -= 1
+end
+
+puts "Done!"
---
+++ loops_iterators/samples.rb
@@ -0,0 +1,8 @@
+x = 42 
+loop do
+  puts x
+  x = 2
+  break
+end 
+
+puts x
